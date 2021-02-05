f13801ed0a0d5eed05c9de54ca4c74d4fc435f03
mina gerges <minagerges4@gmail.com>
2021-02-05 19:13:13 +0200

    add new global message

--- lib/global/messages.dart
+++ lib/global/messages.dart
@@ -5,6 +5,7 @@ abstract class GlobalMessages {
   static const String Loading = 'Loading...';
   static const String Error = 'An Error Occurred';
   static const String UnknownError = 'Unknown Error';
+  static const String ArticleDetailPageTitle = 'Article';
   static const String ArticleDetailTitle = 'Article title';
   static const String NoArticlesFound = 'No Articles found';
   static const String NoInternetConnection = 'No Internet connection';
